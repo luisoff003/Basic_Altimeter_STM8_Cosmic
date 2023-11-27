0eeebf3882e4ab01fdfe332ef1e156329d0cc765
Luis Sanchez <luison003@gmail.com>
2023-11-26 18:06:10 -0600

    Bring up barometer
    - Added I2C libraries, CLK configuration.

--- main.c
+++ main.c
@@ -8,18 +8,12 @@
 
 #include "stm8s.h"
 #include "stm8s_gpio.h"
- #include "stm8s_serial.h" //https://github.com/CircuitDigest/STM8S103F3_SPL/blob/master/stm8s103%20Libraries/stm8s103_Serial.h
+#include "stm8s_serial.h" //https://github.com/CircuitDigest/STM8S103F3_SPL/blob/master/stm8s103%20Libraries/stm8s103_Serial.h
+#include "stm8s_i2c.h" 
+#include "stm8s_clk.h"
  
-void delay (int ms) //Function Definition 
-{
-	int i =0 ;
-	int j=0;
-	for (i=0; i<=ms; i++)
-	{
-		for (j=0; j<120; j++) // Nop = Fosc/4
-		_asm("nop"); //Perform no operation //assembly code  
-	}
-}
+void delay (int ms); 
+void I2CSetup(void);
 
 
 main()
@@ -27,6 +21,7 @@ main()
 	Serial_begin(9600); //Initialize Serial communication at 9600 baud rate
 	GPIO_DeInit(GPIOC); //prepare Port C for working 
 	GPIO_DeInit(GPIOD); //prepare Port C for working 
+
 	//Declare PC4  as output pull up pin
 	GPIO_Init(GPIOC, GPIO_PIN_4, GPIO_MODE_OUT_PP_LOW_SLOW); 
 	//GPIO_Init(GPIOD, GPIO_PIN_4, GPIO_MODE_OUT_PP_LOW_SLOW); 
@@ -41,4 +36,22 @@ main()
 		delay(1000);
 	}
 	
+}
+
+ void delay (int ms)//Function Definition 
+{
+	int i =0 ;
+	int j=0;
+	for (i=0; i<=ms; i++)
+	{
+		for (j=0; j<120; j++) // Nop = Fosc/4
+		_asm("nop"); //Perform no operation //assembly code  
+	}
+}
+
+
+void I2CSetup(void){
+	u8 Input_Clock = 0;
+  	Input_Clock = CLK_GetClockFreq()/1000000;
+// 	I2C_Init(I2C_Speed, MLX90614_I2CADDR, I2C_DUTYCYCLE_2, I2C_ACK_CURR, I2C_ADDMODE_7BIT, Input_Clock);
 }

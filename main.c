ef30884e6a559496aa154db49cf7dae39a00568a
Luis Sanchez <luison003@gmail.com>
2023-11-29 16:56:40 -0600

    I2C includes to the project

--- main.c
+++ main.c
@@ -11,6 +11,16 @@
 #include "stm8s_serial.h" //https://github.com/CircuitDigest/STM8S103F3_SPL/blob/master/stm8s103%20Libraries/stm8s103_Serial.h
 #include "stm8s_i2c.h" 
 #include "stm8s_clk.h"
+
+/* Check this documentation/implementation
+https://github.com/iNavFlight/inav/pull/5028
+https://datasheet.lcsc.com/lcsc/2101201914_Goertek-SPL06-001_C2684428.pdf
+
+*/
+
+#define I2C_SPL06_001_ADDRESS 0x77			
+
+
  
 void delay (int ms); 
 void I2CSetup(void);
@@ -53,5 +63,15 @@ main()
 void I2CSetup(void){
 	u8 Input_Clock = 0;
   	Input_Clock = CLK_GetClockFreq()/1000000;
-// 	I2C_Init(I2C_Speed, MLX90614_I2CADDR, I2C_DUTYCYCLE_2, I2C_ACK_CURR, I2C_ADDMODE_7BIT, Input_Clock);
+ 	I2C_Init(I2C_Speed, I2C_SPL06_001_ADDRESS, I2C_DUTYCYCLE_2, I2C_ACK_CURR, I2C_ADDMODE_7BIT, Input_Clock);
+}
+
+void I2C_Write(){
+	/*  sending the slave address in write mode (RW=’0’) 
+	111011X0 (‘X’ is determined by state of SDO pin. our case will be 0x77  or  for complete i2c message 11101110 */
+
+	/*Then the master sends pairs of register addresses
+	and register data. The transaction is ended by a stop condition.*/
+
+
 }
